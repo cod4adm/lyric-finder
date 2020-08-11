035c98119a1418f76bf2f85ee84e19530ed301e9
cod4adm <57834361+cod4adm@users.noreply.github.com>
2020-08-11 23:34:26 +0300

    deploy

---
+++ precache-manifest.ec6088872c64233d4fcee42c177e353a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7afdb527276ca544658f8b2465216ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bcde6e3f8eab60fa185",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b4a5b97bf86984024bbb",
+    "url": "/static/js/2.e4e5edb0.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.e4e5edb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcde6e3f8eab60fa185",
+    "url": "/static/js/main.42f04f64.chunk.js"
+  },
+  {
+    "revision": "eb9b1e31ffd5e8579188",
+    "url": "/static/js/runtime-main.5dbf9bbe.js"
+  }
+]);
